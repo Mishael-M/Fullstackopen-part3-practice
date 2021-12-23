73c859d14a22f22fa96ccbd3355666641dfc2070
Mishael <magsanoc@live.com>
2021-12-23 14:08:11 -0500

    Added frontend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { request, response } = require('express');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
 let notes = [
@@ -24,10 +25,6 @@ let notes = [
     important: true,
   },
 ];
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-});
 
 app.get('/api/notes', (request, response) => {
   response.json(notes);
